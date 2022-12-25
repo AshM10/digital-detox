60e861d2e173eac081ab838946ff0ef403798243
Ash Moreno <ashterfuentes@gmail.com>
2022-12-25 15:32:10 -0600

    explain what generator file does

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import Data from "./Data";
 
+// This code defines a function called Generator that is a React component.
+// The Generator function uses the useState hook to define a state variable called currentQuote
+// and a function called setCurrentQuote to update the value of currentQuote.
+
 function Generator() {
   const [currentQuote, setCurrentQuote] = useState(null);
+
+  // The Generator component renders a button element that, when clicked, calls the
+  // selectRandomQuote function. The selectRandomQuote function generates a random number
+  // between 0 and the length of the Data array, and uses the setCurrentQuote function to
+  // set the value of currentQuote to the element at the randomly generated index of the
+  // Data array.
 
   function selectRandomQuote() {
     const randomIndex = Math.floor(Math.random() * Data.length);
     setCurrentQuote(Data[randomIndex]);
   }
+
+  // The Generator component also renders a p element that displays the quote and author
+  // properties of the currentQuote object, if it exists. The currentQuote object and
+  // its properties are imported from a separate file called Data.
 
   return (
     <div className="Generator">
@@ -21,5 +35,8 @@ function Generator() {
     </div>
   );
 }
+
+// Finally, the Generator component is exported, so that it can be used in other
+// parts of the application.
 
 export default Generator;
