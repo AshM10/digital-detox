012fd86b32ca4ce3600a9751d427c7e25ee171ca
Ash Moreno <ashterfuentes@gmail.com>
2022-12-25 22:11:56 -0600

    feat: add more data

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,6 +3,9 @@ import Folder from "./assets/folder.png";
 import Clock from "./assets/clock.png";
 import Selfie from "./assets/selfie.png";
 import Words from "./assets/words.png";
+import Nap from "./assets/nap.png";
+import Memory from "./assets/memory.png";
+import Star from "./assets/star.png";
 
 const quotes = [
   {
@@ -10,6 +13,24 @@ const quotes = [
       "Remove yourself from online groups that you don't need to be in, so you get less notifications.",
     author: "Cross",
     image: Cross,
+  },
+  {
+    quote:
+      "Star gaze. Observe the stars and the moon, you might see a shooting star!",
+    author: "Star",
+    image: Star,
+  },
+  {
+    quote:
+      "Try and think back to your very first memory, where were you, who you were with and how did you feel?",
+    author: "Memory",
+    image: Memory,
+  },
+  {
+    quote:
+      "Have a power nap. Research shows that a quick break can stop our brains from being overwhelmed.",
+    author: "Eye Mask",
+    image: Nap,
   },
   {
     quote: "Organize and de-clutter your desktop and folders.",
